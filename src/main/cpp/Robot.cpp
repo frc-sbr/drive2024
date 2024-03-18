02fe16da7d8eff673052ff050a31ee5fe9b8177d
Rishabh Goel <rishabhgoel@Rishabhs-Air.localdomain>
2024-03-17 23:19:43 -0700

    Fully working teleop code. Just need to test Auton. Also need amp preset.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -12,29 +12,51 @@
 void Robot::RobotInit() {
   m_leftMotor1.AddFollower(m_leftMotor2);
   m_rightMotor1.AddFollower(m_rightMotor2);
-  // shootMotor2.Follow(shootMotor1);
 
   m_armEncoder.SetDistancePerPulse(1.0 / 2048);
   m_armEncoder.Reset();
+  m_armController.SetIZone(0.015);
 
   m_rightMotor1.SetInverted(true);
-  rightSlammer.SetInverted(true);
+
+  rightSlammer.SetInverted(false);
+  leftSlammer.SetInverted(true);
 }
 
 void Robot::RobotPeriodic() {
+
 }
 
 // AUTON  ==================================================================
 void Robot::AutonomousInit() {
+  startTime = frc::Timer::GetFPGATimestamp();
 }
 
 void Robot::AutonomousPeriodic() {
+  units::second_t currentTime = frc::Timer::GetFPGATimestamp();
+  double elapsedTime = currentTime.value() - startTime.value();
+
+  if (elapsedTime < 2.5){
+    Shoot(startTime);
+  } else {
+    shootMotor1.Set(0);
+    shootMotor2.Set(0);
+  }
+
+  if (elapsedTime < 6.5 && elapsedTime > 2.5){
+    Drive(0.5, 0, false);
+  } else {
+    Drive(0, 0, false);
+  }
+
 }
 
 // TELEOP ==================================================================
 void Robot::TeleopInit() {
   shootMotor1.Set(0);
   shootMotor2.Set(0);
+
+  isShooting = false;
 }
 
 void Robot::TeleopPeriodic() {
@@ -44,19 +66,34 @@ void Robot::TeleopPeriodic() {
     driveController.GetRawButton(1));
 
   // ARM ====================================================================
-  // if (opController.GetRawButton(5)){
-  //   setpoint = 0;
-  // } else if (opController.GetRawButton(6)){
-  //   setpoint = 0.5;
-  // } else if (opController.GetRawButton(7)){
-  //   setpoint = -0.1;
-  // } else if (opController.GetRawButton(8)){
-  //   setpoint = 0.3;
-  // }
+  if (opController.GetRawButtonPressed(1)){
+    setpoint = 0;
+  } else if (opController.GetRawButtonPressed(4)){
+    // amp
+    setpoint = 0.129;
+  } else if (opController.GetRawButtonPressed(2)){
+    // akimbo 
+    setpoint = 0.267;
+  } else if (opController.GetRawButtonPressed(3)){
+    // intake
+    setpoint = 0.058;
+  }
+
+  if (abs(opController.GetRawAxis(1)) > 0.01){
+    setpoint = m_armEncoder.GetDistance();
+    doPid = false;
+  }else 
+    doPid = true;
+
   RotateArm(opController.GetRawAxis(1));
 
+  if (opController.GetRawButton(9) && opController.GetRawButton(10)){
+    setpoint = 0;
+    m_armEncoder.Reset();
+  }
+
    // MECHANISM ==================================================================== 
-  if (opController.GetRawButtonPressed(1)){
+  if (opController.GetRawAxis(2) == 1 && opController.GetRawAxis(3) == 1 && !isShooting){
     isShooting = true;
     startTime = frc::Timer::GetFPGATimestamp();
   }
@@ -69,6 +106,7 @@ void Robot::TeleopPeriodic() {
 
   // TELEMETRY ====================================================================
   frc::SmartDashboard::PutNumber("Arm Angle", m_armEncoder.GetDistance());
+  frc::SmartDashboard::PutNumber("Current Setpoint", setpoint);
 }
 
 void Robot::Drive(double xSpeed, double zRotation, bool turnInPlace){
@@ -87,16 +125,24 @@ void Robot::Drive(double xSpeed, double zRotation, bool turnInPlace){
 }
 
 void Robot::RotateArm(double speed){
-  if (abs(speed) < 0.1){
-    rightSlammer.Set(0);
-    leftSlammer.Set(0);
-    return;
+  if (doPid){
+    double output = m_armController.Calculate(m_armEncoder.GetDistance(), setpoint);
+    if (output > 1){
+      output = 1;
+    } else if (output < -1){
+      output = -1;
+    }
+    rightSlammer.Set(output);
+    leftSlammer.Set(output);
+    } else {
+      if (abs(speed) < 0.1){
+        rightSlammer.Set(0);
+        leftSlammer.Set(0);
+        return;
+      }
+      rightSlammer.Set(speed);
+      leftSlammer.Set(speed);
   }
-  rightSlammer.Set(speed);
-  leftSlammer.Set(speed);
-
-  // PID
-  // rightSlammer.Set(m_armController.Calculate(m_armEncoder.GetDistance(), setpoint));
 }
 
 void Robot::Shoot(units::second_t startTime){
